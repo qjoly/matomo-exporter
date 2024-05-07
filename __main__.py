047523c7bc1154031e360e2eebc886ddb8d58936
QJoly <quentinj@une-pause-cafe.fr>
2024-05-07 13:21:16 +0200

    chore: Add Docker-related files and metrics.py

--- __main__.py
+++ __main__.py
@@ -17,16 +17,16 @@ import argparse
 from prometheus_client import start_http_server
 from metrics import (
     NUMBER_SITES,
-    NUMBER_VISITS_YESTERDAY,
-    NUMBER_UNIQ_VISITORS_YESTERDAY,
-    NUMBER_VISITS_CURRENT,
-    NUMBER_UNIQ_VISITORS_CURRENT,
-    NUMBER_BOUNCING_RATE_CURRENT,
-    NUMBER_ACTIONS_CURRENT,
-    NUMBER_BOUNCING_RATE_YESTERDAY,
-    NUMBER_ACTIONS_YESTERDAY,
-    NUMBER_VISITS_PER_PAGE_CURRENT_MONTH,
-    NUMBER_VISITS_PAR_PAGE_CURRENT_YEAR,
+    NUMBER_VISITS,
+    NUMBER_UNIQ_VISITORS,
+    NUMBER_VISITS,
+    NUMBER_UNIQ_VISITORS,
+    NUMBER_BOUNCING_RATE,
+    NUMBER_ACTIONS,
+    NUMBER_BOUNCING_RATE,
+    NUMBER_ACTIONS,
+    NUMBER_VISITS_PER_PAGE,
+    NUMBER_VISITORS_PER_OS_VERSION,
 )
 import logging
 
@@ -173,7 +173,6 @@ def get_most_visited_pages(site_id, period):
     data = get_all_pages(site_id, period)
 
     dict_data = {}
-    
     def print_data(data, parent=""):
         if type(data) is dict and data.get("subtable"):
             print_data(data.get("subtable"), parent + "/" + data.get("label"))
@@ -205,57 +204,88 @@ def update_metrics():
             site_name = get_name_of_site(site_id)
             site_data_yesterday = get_number_of_visits_yesterday(site_id)
 
-            NUMBER_VISITS_YESTERDAY.labels(site_name).set(
+            NUMBER_VISITS.labels(site_name, "previous_day").set(
                 site_data_yesterday.get("nb_visits")
             )
-            NUMBER_UNIQ_VISITORS_YESTERDAY.labels(site_name).set(
+            NUMBER_UNIQ_VISITORS.labels(site_name, "previous_day").set(
                 site_data_yesterday.get("nb_uniq_visitors")
             )
-            NUMBER_BOUNCING_RATE_YESTERDAY.labels(site_name).set(
+            NUMBER_BOUNCING_RATE.labels(site_name, "previous_day").set(
                 site_data_yesterday.get("bounce_count")
             )
-            NUMBER_ACTIONS_YESTERDAY.labels(site_name).set(
+            NUMBER_ACTIONS.labels(site_name, "previous_day").set(
                 site_data_yesterday.get("nb_actions")
             )
 
             site_data_current = get_number_of_visits_current(site_id)
-            NUMBER_VISITS_CURRENT.labels(site_name).set(
+            NUMBER_VISITS.labels(site_name, "now").set(
                 site_data_current.get("nb_visits")
             )
-            NUMBER_UNIQ_VISITORS_CURRENT.labels(site_name).set(
+            NUMBER_UNIQ_VISITORS.labels(site_name, "now").set(
                 site_data_current.get("nb_uniq_visitors")
             )
-            NUMBER_BOUNCING_RATE_CURRENT.labels(site_name).set(
+            NUMBER_BOUNCING_RATE.labels(site_name, "now").set(
                 site_data_current.get("bounce_count")
             )
 
-            NUMBER_ACTIONS_CURRENT.labels(site_name).set(
+            NUMBER_ACTIONS.labels(site_name, "now").set(
                 site_data_current.get("nb_actions")
             )
+
+            daily_visited_pages = get_most_visited_pages(site_id, ma.period.day)
+            for page in daily_visited_pages.keys():
+                NUMBER_VISITS_PER_PAGE.labels(site_name, page, "day").set(
+                    daily_visited_pages[page]
+                )
 
             monthly_visited_pages = get_most_visited_pages(site_id, ma.period.month)
 
             for page in monthly_visited_pages.keys():
-                NUMBER_VISITS_PER_PAGE_CURRENT_MONTH.labels(site_name, page).set(
+                NUMBER_VISITS_PER_PAGE.labels(site_name, page, "month").set(
                     monthly_visited_pages[page]
                 )
 
             yearly_visited_pages = get_most_visited_pages(site_id, ma.period.year)
 
             for page in yearly_visited_pages.keys():
-                NUMBER_VISITS_PAR_PAGE_CURRENT_YEAR.labels(site_name, page).set(
+                NUMBER_VISITS_PER_PAGE.labels(site_name, page, "year").set(
                     yearly_visited_pages[page]
                 )
+
+            for os in visitors_details_os_versions(site_id, ma.period.day):
+                NUMBER_VISITORS_PER_OS_VERSION.labels(
+                    site_name, os.get("label"), "now"
+                ).set(os.get("nb_visits"))
+
+            for os in visitors_details_os_versions(site_id, ma.period.month):
+                NUMBER_VISITORS_PER_OS_VERSION.labels(
+                    site_name, os.get("label"), "month"
+                ).set(os.get("nb_visits"))
+
+            for os in visitors_details_os_versions(site_id, ma.period.year):
+                NUMBER_VISITORS_PER_OS_VERSION.labels(
+                    site_name, os.get("label"), "year"
+                ).set(os.get("nb_visits"))
 
         except FileExistsError as e:
             logging.error(f"Error getting data for site {site_id}, {e}")
             continue
 
 
+def visitors_details_os_versions(site_id, period):
+    """Get the details of OS used by visitors for a given site and period"""
+    pars = ma.format.json | ma.idSite.one_or_more(site_id) | ma.date.today | period
+    qry_result = api.DevicesDetection().getOsVersions(pars)
+
+    return qry_result.json()
+
+
 if __name__ == "__main__":
     """Main function"""
     start_http_server(port=PORT, addr=IP)
     logging.info("Server started, listening on IP: %s:%s", IP, PORT)
+
+    visitors_details_os_versions(1, ma.period.month)
 
     while True:
         update_metrics()
--- metrics.py
+++ metrics.py
@@ -1,42 +1,27 @@
 from prometheus_client import Gauge
 
 NUMBER_SITES = Gauge("number_of_sites", "Number of sites")
-NUMBER_VISITS_YESTERDAY = Gauge(
-    "number_of_visits_yesterday", "Number of visits", ["site_name"]
-)
-NUMBER_UNIQ_VISITORS_YESTERDAY = Gauge(
-    "number_uniq_visitors_yesterday", "Number of visits", ["site_name"]
-)
-NUMBER_VISITS_CURRENT = Gauge(
-    "number_of_visits_current_day", "Number of visits", ["site_name"]
-)
-NUMBER_UNIQ_VISITORS_CURRENT = Gauge(
-    "number_uniq_visitors_current_day", "Number of visits", ["site_name"]
+
+NUMBER_VISITS = Gauge("number_of_visits_current_day", "Number of visits", ["site_name", "period"])
+NUMBER_UNIQ_VISITORS = Gauge(
+    "number_uniq_visitors", "Number of visits", ["site_name", "period"]
 )
 
-NUMBER_BOUNCING_RATE_CURRENT = Gauge(
-    "percentage_bouncing_rate_current_day", "Bouncing rate", ["site_name"]
+NUMBER_BOUNCING_RATE = Gauge(
+    "number_bouncing_rate", "Bouncing rate", ["site_name", "period"]
 )
-NUMBER_ACTIONS_CURRENT = Gauge(
-    "number_of_actions_current_day", "Number of actions", ["site_name"]
-)
-
-NUMBER_BOUNCING_RATE_YESTERDAY = Gauge(
-    "number_bouncing_rate_yesterday", "Bouncing rate", ["site_name"]
-)
-NUMBER_ACTIONS_YESTERDAY = Gauge(
-    "number_of_actions_yesterday", "Number of actions", ["site_name"]
+NUMBER_ACTIONS = Gauge(
+    "number_of_actions", "Number of actions", ["site_name", "period"]
 )
 
-NUMBER_VISITS_PER_PAGE_CURRENT_MONTH = Gauge(
-    "number_of_visited_pages_current_month",
-    "Number of visited pages",
-    ["site_name", "page"],
+NUMBER_VISITS_PER_PAGE = Gauge(
+    "number_of_visit_per_page",
+    "Number of visits per page",
+    ["site_name", "page", "period"],
 )
 
-NUMBER_VISITS_PAR_PAGE_CURRENT_YEAR = Gauge(
-    "number_of_visited_pages_current_year",
-    "Number of visited pages",
-    ["site_name", "page"],
+NUMBER_VISITORS_PER_OS_VERSION = Gauge(
+    "number_of_visitors_per_os_version",
+    "Number of visitors per OS version",
+    ["site_name", "os_version", "period"],
 )
-
